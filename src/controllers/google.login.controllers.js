65e697ea05dbf9cf95e4dffcc0aeb2f899e09995
nayeemmehedi <boyneel79@gmail.com>
2024-05-27 03:23:43 +0600

    google issue

--- src/controllers/google.login.controllers.js
+++ src/controllers/google.login.controllers.js
@@ -3,6 +3,29 @@ const { ApiResponse } = require("../utils/apiHandler.js");
 // const serviceAccount = require("../utils/firebase.json");
 const signup = require("../models/signup.models.js");
 const serviceAccountValue = require("../utils/firebase.jsx");
+const jwt = require("jsonwebtoken");
+
+function generateAccessToken(data) {
+  return jwt.sign(
+    {
+      // _id: data._id,
+      email: data.email,
+    },
+    process.env.accees_token_secrat,
+    { expiresIn: "15m" }
+  );
+}
+
+function generateRefreshToken(data) {
+  return jwt.sign(
+    {
+      // _id: data._id,
+      email: data.email,
+    },
+    process.env.refresh_token_secrat,
+    { expiresIn: "30d" }
+  );
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountValue),
@@ -20,27 +43,31 @@ const verifyIdToken = async (idToken) => {
   }
 };
 
-
 const goggleLogin = async (req, res) => {
   try {
+    console.log("start login");
+    //succefuuly get token
     const user_token = await verifyIdToken(req.body.idToken);
     let firebase_user_info = await signup.findOne({ email: user_token.email });
-   
-   
+    console.log("firebase_user_info");
 
     if (!firebase_user_info) {
-
       let newUser = new signup({
         email: user_token.email,
         username: user_token.displayName,
         googleId: user_token,
         emailVerified: user_token.emailVerified,
       });
-      const accessToken = newUser.accessTokenMethods(newUser);
-      const refreshToken = newUser.refreshTokenMethods(newUser);
+      const accessToken = await newUser.accessTokenMethods(newUser);
+      const refreshToken = await newUser.refreshTokenMethods(newUser);
+
+      console.log("Access token new", accessToken);
+
       newUser.refreshToken = refreshToken;
       await newUser.save();
-      let withoutRefeshTokenUser = await signup.findOne({ email: user_token.email });
+      let withoutRefeshTokenUser = await signup.findOne({
+        email: user_token.email,
+      });
       const options = { httpOnly: true, secure: true };
       const responseData = new ApiResponse(
         200,
@@ -61,28 +88,51 @@ const goggleLogin = async (req, res) => {
         .cookie("refreshToken", refreshToken, options)
         .json(responseData);
     } else {
-      const options = { httpOnly: true, secure: true };
-      const responseData = new ApiResponse(
-        200,
-        {
-          accessToken: firebase_user_info.accessToken,
-          refreshToken: firebase_user_info.refreshToken,
-          userName: firebase_user_info.username,
-          userEmail: firebase_user_info.email,
-          role: firebase_user_info.role,
-          pending: firebase_user_info.pending,
-          id: firebase_user_info._id,
-        },
-        true
-      );
-      res
-        .status(200)
-        .cookie("accessToken", accessToken, options)
-        .cookie("refreshToken", refreshToken, options)
-        .json(responseData);
+      console.log("next");
+
+      let newUser = new signup(firebase_user_info);
+
+      const accessToken = generateAccessToken(firebase_user_info);
+      const refreshToken = generateRefreshToken(firebase_user_info);
+
+
+
+        const updatedProject = await signup.findByIdAndUpdate(
+          { _id: firebase_user_info._id },
+          { $set: {refreshToken:refreshToken} },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+
+        if (!updatedProject) {
+          return res.status(404).json({ message: "Project not found" });
+        }
+
+        const options = { httpOnly: true, secure: true };
+        const responseData = new ApiResponse(
+          200,
+          {
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+            userName: firebase_user_info.username,
+            userEmail: firebase_user_info.email,
+            role: firebase_user_info.role,
+            pending: firebase_user_info.pending,
+            id: firebase_user_info._id,
+          },
+          true
+        );
+        res
+          .status(200)
+          .cookie("accessToken", accessToken, options)
+          .cookie("refreshToken", refreshToken, options)
+          .json(responseData);
+      
     }
   } catch (error) {
-    res.status(500).json({ error: error.toString() ,message:error.message});
+    res.status(500).json({ error: error.toString(), message: error.message });
   }
 };
 
